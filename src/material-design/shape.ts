723e31723122bfd21bed91fd5cc00757d57dda93
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-03 13:07:51 -0300

    Add shape

---
+++ src/material-design/shape.ts
@@ -0,0 +1,18 @@
+
+export type ShapeKey = "none" | "extraSmall" | "small" | "medium"
+    | "large" | "extraLarge" | "full"
+
+export type Shape = {
+    [key in ShapeKey]: number
+}
+
+
+export const shape: Shape = {
+    none: 0,
+    extraSmall: 4,
+    small: 8,
+    medium: 12,
+    large: 16,
+    extraLarge: 28,
+    full: 1_000_000_000,
+}
